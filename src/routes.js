0b1a36ad8ccc942b6e9f102d976d023704c7c050
Proctux <luiz.ado@hotmail.com>
2021-09-22 23:38:29 -0300

    chore: Change some route configuration and global css

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Switch } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import App from './App'
+import WebWhatsapp from 'views/web-whatsapp'
 
-const Routes = () => (
-  <Switch>
-    <App path="/" />
-  </Switch>
-)
+const Routes = () => {
+  return (
+    <Switch>
+      <App>
+        <Route path="/">
+          <WebWhatsapp />
+        </Route>
+      </App>
+    </Switch>
+  )
+}
 
 export default Routes
